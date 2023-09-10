180c7f21279bd2d97154f1d4ed9c33166acfc612
Clare Constantine <clare.constantine@gmail.com>
2023-09-10 01:18:57 -0700

    title screen

--- src/game.ts
+++ src/game.ts
@@ -9,39 +9,58 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image('background', 'assets/images/title_background.png');
+        this.load.image('logo', 'assets/images/desert-dash-logo.png');
+        this.load.spritesheet('cactus',
+            'assets/images/cactus_round_medium.png',
+            { frameWidth: 128, frameHeight: 128 }
+        );
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        this.add.image(400, 300, 'background');
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
+        const logo = this.add.image(400, 150, 'logo');
 
         this.tweens.add({
             targets: logo,
-            y: 350,
+            y: 250,
             duration: 1500,
-            ease: 'Sine.inOut',
             yoyo: true,
             repeat: -1
         })
+
+        this.anims.create({
+            key: 'bounce',
+            frames: this.anims.generateFrameNumbers('cactus', { start: 0, end: 3 }),
+            frameRate: 5,
+            // loop the animation
+            repeat: -1
+        });
+
+        this.add.group({
+            key: 'cactus',
+            repeat: 2,
+            setXY: { x: 240, y: 430, stepX: 320 }
+        }).playAnimation('bounce');
+
+        this.add.group({
+          key: 'cactus',
+          repeat: 3,
+          setXY: { x: 80, y: 400, stepX: 320 }
+        }).playAnimation('bounce');
+
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
+    backgroundColor: '#56C1D8',
     width: 800,
     height: 600,
-    scene: Demo
+    scene: Demo,
+    scale: {autoCenter: Phaser.Scale.CENTER_BOTH}
 };
 
 const game = new Phaser.Game(config);
